b584ccf6a20a039029f606f2c6552ad7e6eed49c
Nikita Volkov <2545442@gmail.com>
2018-11-01 23:58:23 +0200

    add info comment to deploy.bat

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,2 @@
+rem npm install -g angular-cli-ghpages
 @call ng build --prod --base-href "https://NikitaVolkov87.github.io/Commits_Info_App-Angular6-/" && ngh && echo Done! && timeout 5 && start https://NikitaVolkov87.github.io/Commits_Info_App-Angular6-
